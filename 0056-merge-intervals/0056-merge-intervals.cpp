76d115ac8848cb798a0571c3733664088e9fb215
Aniket Pandey <163867028+aniketpandey2705@users.noreply.github.com>
2026-09-17 19:52:05 +0530

    Time: 6 ms (45.64%), Space: 23.8 MB (60.42%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,21 +4,35 @@ public:
         vector<vector<int>> ans;
         sort(intervals.begin(), intervals.end());
         int n = intervals.size();
-        for (int i = 0; i < n; i++) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if (!ans.empty() && end <= ans.back()[1])
-                continue;
-            for (int j = i + 1; j < n; j++) {
-                if (end >= intervals[j][0]) {
-                    end = max(end, intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+        for (int i = 0; i < n; i++) {;
+            if( ans.empty() || ans.back()[1] < intervals[i][0]){
+                ans.push_back(intervals[i]);
             }
-            ans.push_back({start,end});
+            else{
+                ans.back()[1]= max(ans.back()[1], intervals[i][1]);
+            }
         }
         return ans;
     }
 };
+
+        //brute
+        // vector<vector<int>> ans;
+        // sort(intervals.begin(), intervals.end());
+        // int n = intervals.size();
+        // for (int i = 0; i < n; i++) {
+        //     int start = intervals[i][0];
+        //     int end = intervals[i][1];
+        //     if (!ans.empty() && end <= ans.back()[1])
+        //         continue;
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (end >= intervals[j][0]) {
+        //             end = max(end, intervals[j][1]);
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        //     ans.push_back({start,end});
+        // }
+        // return ans;
